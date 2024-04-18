cc863737749795f7a5c6d1521c5dbad7b196931d
tanmay4l <mrspot67@gmail.com>
2024-04-18 20:17:55 +0530

    Update generate-sitemap.mjs

--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -37,7 +37,7 @@ async function main() {
   const bookSlugs = getAllBookSlugs();
   const writingSlugs = getAllWritingSlugs();
   const engineeringSlugs = getAllEngineeringSlugs();
-  const allSlugs = [...bookSlugs, ...writingSlugs, ...engineeringSlugs];
+  const allSlugs = [...bookSlugs, ...writingSlugs, ];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
